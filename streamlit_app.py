7e3ae164ea82db85c69c924b50ef6e02b3103722
rsn183514 <111360973+rsn183514@users.noreply.github.com>
2022-08-17 13:20:16 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,7 @@ st.header("Fruityvice Fruit Advice!")
 
 # new section to display fruityvice api response
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ "kiwi")
 st.text(fruityvice_response.json())
 
 
